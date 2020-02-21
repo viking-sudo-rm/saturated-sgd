95cbb55cd324bed74cf62d871c9c9002e2dce322
Will Merrill <vikingarnir.will@gmail.com>
2020-02-21 14:56:36 -0800

    Updated magnitude prunable RNN.

--- scripts/prune-eval.sh
+++ scripts/prune-eval.sh
@@ -1,12 +1,23 @@
-
 echo "p=$p"
 
-allennlp evaluate /tmp/willm/saturated-sgd/prune-agree/0.0/model.tar.gz \
-    /net/nfs.corp/allennlp/willm/data/rnn_agr_simple/numpred.val \
-    -o "{\"model.seq2seq_encoder.percent\":$p, \"model.seq2seq_encoder.prune_saturated\": false}" \
-    --include-package=src
+# Agreement.
+# allennlp evaluate /tmp/willm/saturated-sgd/prune-agree/0.0/model.tar.gz \
+#     /net/nfs.corp/allennlp/willm/data/rnn_agr_simple/numpred.val \
+#     -o "{\"model.seq2seq_encoder.percent\": $p, \"model.seq2seq_encoder.prune_saturated\": false}" \
+#     --include-package=src
 
+# Language modeling.
 # allennlp evaluate /tmp/willm/saturated-sgd/lm/penn/adamw-0.5-1.2e-6/model.tar.gz \
 #     /net/nfs.corp/allennlp/willm/data/penn/valid.txt \
-#     -o "{\"model.contextualizer.percent\": $p}" \
+#     -o "{\"model.contextualizer.percent\": $p, \"model.contextualizer.prune_saturated\": true}" \
 #     --include-package=src
+
+# Sentiment.
+allennlp evaluate /tmp/willm/saturated-sgd/sentiment/basic/model.tar.gz \
+    /net/nfs.corp/allennlp/willm/data/stanford/trees/dev.txt \
+    --include-package=src \
+    -o "{
+        \"model.seq2seq_encoder.percent\": $p,
+        \"model.seq2seq_encoder.prune_saturated\": false,
+        \"model.seq2seq_encoder.random_baseline\": false,
+    }"
