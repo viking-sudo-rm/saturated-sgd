e8f947d4199dc9f2107ec27c96820b608ef5ed8c
Vivek Ramanujan <vivekr@allenai.org>
2020-07-02 15:20:54 -0700

    format

--- src/training/optim/floor_sgd.py
+++ src/training/optim/floor_sgd.py
@@ -6,11 +6,12 @@ from allennlp.training.optimizers import Optimizer, make_parameter_groups
 
 EPS = torch.finfo(torch.float64).eps
 
+
 class FloorSGD(optim.SGD):
     def __init__(self, *args, min_step: float = 1e-1, **kwargs):
         super().__init__(*args, **kwargs)
         self.min_step = min_step
-    
+
     @torch.no_grad()
     def step(self, closure=None):
         """Performs a single optimization step.
@@ -24,12 +25,12 @@ class FloorSGD(optim.SGD):
                 loss = closure()
 
         for group in self.param_groups:
-            weight_decay = group['weight_decay']
-            momentum = group['momentum']
-            dampening = group['dampening']
-            nesterov = group['nesterov']
+            weight_decay = group["weight_decay"]
+            momentum = group["momentum"]
+            dampening = group["dampening"]
+            nesterov = group["nesterov"]
 
-            for p in group['params']:
+            for p in group["params"]:
                 if p.grad is None or not p.requires_grad:
                     continue
 
@@ -37,23 +38,25 @@ class FloorSGD(optim.SGD):
                 # TODO: Apply across each row in each matrix??? Don't treat all matrices the same.
                 d_p = p.grad
                 norm = p.grad.norm(p=2, dim=-1, keepdim=True)
-                d_p = torch.where(norm < self.min_step, d_p * self.min_step / (norm + EPS), d_p)
+                d_p = torch.where(
+                    norm < self.min_step, d_p * self.min_step / (norm + EPS), d_p
+                )
 
                 if weight_decay != 0:
                     d_p = d_p.add(p, alpha=weight_decay)
                 if momentum != 0:
                     param_state = self.state[p]
-                    if 'momentum_buffer' not in param_state:
-                        buf = param_state['momentum_buffer'] = torch.clone(d_p).detach()
+                    if "momentum_buffer" not in param_state:
+                        buf = param_state["momentum_buffer"] = torch.clone(d_p).detach()
                     else:
-                        buf = param_state['momentum_buffer']
+                        buf = param_state["momentum_buffer"]
                         buf.mul_(momentum).add_(d_p, alpha=1 - dampening)
                     if nesterov:
                         d_p = d_p.add(buf, alpha=momentum)
                     else:
                         d_p = buf
 
-                p.add_(d_p, alpha=-group['lr'])
+                p.add_(d_p, alpha=-group["lr"])
 
         return loss
 
@@ -80,5 +83,5 @@ class FloorSgdOptimizer(Optimizer, FloorSGD):
             dampening=dampening,
             weight_decay=weight_decay,
             nesterov=nesterov,
-            min_step = min_step,
+            min_step=min_step,
         )
